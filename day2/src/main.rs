cda6d18ea56e0c818e28dd3d378093e34e46f42b
michaelwh <michaelwhodgson@gmail.com>
2018-12-08 15:22:50 +0000

    Day 2 part 2!

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     let lines = include_str!("input.txt").lines().collect::<Vec<_>>();
     println!("Day 2 part 1: {}", checksum(&lines[..]));
+    println!("Day 2 part 2: {}", id_common_letters_enfinder(&lines[..]));
 }
 
 fn finder(num_unique: usize, id: &str) -> bool {
@@ -16,6 +17,40 @@ fn checksum(ids: &[&str]) -> i32 {
     let twos = ids.iter().filter(|&id| finder(2, &id)).count() as i32;
     let threes = ids.iter().filter(|&id| finder(3, &id)).count() as i32;
     twos * threes
+}
+
+fn differ(this: &str, that: &str) -> Option<usize> {
+    assert_eq!(this.len(), that.len());
+
+    let mut pos_different: Option<usize> = None;
+
+    for (i, (this_char, that_char)) in this.chars().zip(that.chars()).enumerate() {
+        if this_char != that_char {
+            if pos_different.is_some() {
+                // more than one letter different
+                return None
+            }
+            pos_different = Some(i);
+        }
+    }
+    pos_different
+}
+
+fn destroyer_of_letter(puny_string: &str, pos: usize) -> String {
+    let mut string_under_destruction = puny_string.to_string();
+    string_under_destruction.remove(pos);
+    string_under_destruction
+}
+
+fn id_common_letters_enfinder(ids: &[&str]) -> String {
+    for this_id in ids.iter() {
+        for that_id in ids.iter() {
+            if let Some(i) = differ(this_id, that_id) {
+                return destroyer_of_letter(this_id, i)
+            }
+        }
+    }
+    "".to_string()
 }
 
 #[test]
@@ -56,4 +91,34 @@ fn given_ids_with_two_and_two_threes_return_2() {
 #[test]
 fn given_ids_with_two_twos_and_two_threes_return_4() {
     assert_eq!(checksum(&["aa", "bb", "aaa", "bbb"]), 4);
+}
+
+#[test]
+fn differ_given_identical_gives_returns_false() {
+    assert_eq!(differ("aaa", "aaa"), None);
+}
+
+#[test]
+fn differ_given_two_different_single_letters_returns_0() {
+    assert_eq!(differ("a", "b"), Some(0));
+}
+
+#[test]
+fn differ_given_two_with_one_letter_different_returns_pos() {
+    assert_eq!(differ("aa", "ab"), Some(1));
+}
+
+#[test]
+fn differ_given_two_with_two_different_returns_none() {
+    assert_eq!(differ("aa", "bc"), None);
+}
+
+#[test]
+fn destroyer_of_letter_removes_picked_letter() {
+    assert_eq!(destroyer_of_letter("fghij", 2), "fgij");
+}
+
+#[test]
+fn id_common_letters_enfinder_finds_thing() {
+    assert_eq!(id_common_letters_enfinder(&["abcde", "fghij", "klmno", "pqrst", "fguij", "axcye", "wvxyz"]), "fgij");
 }
