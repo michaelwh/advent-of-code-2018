fe6b905063d922ef937a38e40cb38748a5a337ce
michaelwh <michaelwhodgson@gmail.com>
2018-12-09 21:22:25 +0000

    Day 3 part 1!

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,46 +1,12 @@
-#[macro_use] extern crate itertools;
-
-use itertools::Itertools;
-
 fn main() {
-    println!("Hello, world!");
+    let lines = include_str!("input.txt").lines().collect::<Vec<_>>();
+    println!("Day 3 part 1: {}", overlapper(&lines[..]));
 }
 
 struct Spec {
     id: u32,
     start: (u32, u32),
     size: (u32, u32)
-}
-
-fn spec_parser_old(spec_string: &str) -> Spec {
-    let mut whitespace_split = spec_string.split_whitespace();
-    let id = whitespace_split.next().unwrap().chars().skip(1).collect::<String>().to_string().parse().unwrap();
-    let rest_of_string = whitespace_split.collect::<String>().to_string();
-    let mut string_iter = rest_of_string.chars();
-    let start_x = string_iter.by_ref().skip_while(|c| *c != '@').skip(1).take_while(|c| *c != ',').collect::<String>().parse().unwrap();
-    let start_y = string_iter.by_ref().take_while(|c| *c != ':').collect::<String>().parse().unwrap();
-    let size_x = string_iter.by_ref().take_while(|c| *c != 'x').collect::<String>().parse().unwrap();
-    let size_y = string_iter.by_ref().collect::<String>().parse().unwrap();
-
-    Spec {
-        id: id,
-        start: (start_x, start_y),
-        size: (size_x, size_y)
-    }
-}
-
-fn spec_parser_newer_old(spec_string: &str) -> Spec {
-    let mut parts = spec_string.split(['#', '@', ',', ':', 'x'].as_ref()).skip(1);
-    let id = parts.next().unwrap().trim().parse().unwrap();
-    let start_x = parts.next().unwrap().trim().parse().unwrap();
-    let start_y = parts.next().unwrap().trim().parse().unwrap();
-    let size_x = parts.next().unwrap().trim().parse().unwrap();
-    let size_y = parts.next().unwrap().trim().parse().unwrap();
-    Spec {
-        id: id,
-        start: (start_x, start_y),
-        size: (size_x, size_y)
-    }
 }
 
 fn spec_parser(spec_string: &str) -> Spec {
@@ -55,12 +21,34 @@ fn spec_parser(spec_string: &str) -> Spec {
 
 fn cells(spec: Spec) -> Vec<(u32, u32)> {
     let mut vector = Vec::new();
-    for x in (spec.start.0..spec.start.0 + spec.size.0) {
-        for y in (spec.start.1..spec.start.1 + spec.size.1) {
+    for x in spec.start.0..spec.start.0 + spec.size.0 {
+        for y in spec.start.1..spec.start.1 + spec.size.1 {
             vector.push((x, y));
         }
     }
     vector
+}
+
+fn overlapper(spec_strings: &[&str]) -> usize {
+    use std::collections::HashMap;
+
+    let mut claimed = HashMap::new();
+    for spec_string in spec_strings {
+        let spec = spec_parser(spec_string);
+        for cell in cells(spec) {
+            match claimed.get(&cell) {
+                Some(&num) => claimed.insert(cell, num + 1),
+                _ => claimed.insert(cell, 1)
+            };
+        }
+    }
+    let mut num_overlapped: usize = 0;
+    for (_cell, num) in claimed {
+        if num > 1 {
+            num_overlapped += 1
+        }
+    }
+    num_overlapped
 }
 
 #[test]
@@ -117,5 +105,45 @@ fn cells_given_2x2_at_0_0() {
 
 #[test]
 fn cells_given_1x1_at_1_3() {
-    assert_eq!(cells(Spec { id: 0, start: (1, 3), size: (1, 1)}), vec![(1, 3)])
+    assert_eq!(cells(Spec { id: 0, start: (1, 3), size: (1, 1)}), vec![(1, 3)]);
+}
+
+#[test]
+fn overlapper_given_example() {
+    assert_eq!(overlapper(&["#1 @ 1,3: 4x4", "#2 @ 3,1: 4x4", "#3 @ 5,5: 2x2"]), 4);
+}
+
+// working, but worse
+
+#[allow(dead_code)]
+fn spec_parser_first_attempt(spec_string: &str) -> Spec {
+    let mut whitespace_split = spec_string.split_whitespace();
+    let id = whitespace_split.next().unwrap().chars().skip(1).collect::<String>().to_string().parse().unwrap();
+    let rest_of_string = whitespace_split.collect::<String>().to_string();
+    let mut string_iter = rest_of_string.chars();
+    let start_x = string_iter.by_ref().skip_while(|c| *c != '@').skip(1).take_while(|c| *c != ',').collect::<String>().parse().unwrap();
+    let start_y = string_iter.by_ref().take_while(|c| *c != ':').collect::<String>().parse().unwrap();
+    let size_x = string_iter.by_ref().take_while(|c| *c != 'x').collect::<String>().parse().unwrap();
+    let size_y = string_iter.by_ref().collect::<String>().parse().unwrap();
+
+    Spec {
+        id: id,
+        start: (start_x, start_y),
+        size: (size_x, size_y)
+    }
+}
+
+#[allow(dead_code)]
+fn spec_parser_second_attempt(spec_string: &str) -> Spec {
+    let mut parts = spec_string.split(['#', '@', ',', ':', 'x'].as_ref()).skip(1);
+    let id = parts.next().unwrap().trim().parse().unwrap();
+    let start_x = parts.next().unwrap().trim().parse().unwrap();
+    let start_y = parts.next().unwrap().trim().parse().unwrap();
+    let size_x = parts.next().unwrap().trim().parse().unwrap();
+    let size_y = parts.next().unwrap().trim().parse().unwrap();
+    Spec {
+        id: id,
+        start: (start_x, start_y),
+        size: (size_x, size_y)
+    }
 }
